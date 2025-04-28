51b93c63b4549d14f1a87d16dd799b649d2156c1
Alejandro Valencia <alejandro@bondly.com>
2025-04-28 14:51:14 -0300

    fix toast style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster />
+        <Toaster richColors />
         <main>{children}</main>
       </body>
     </html>
